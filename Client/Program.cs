2c10f2d6bbcd2def3ba4a9e98ff05374dad75dbc
s21-himesh <himesh@hig.limited>
2023-10-18 16:27:01 +0100

    Sample updated and is working

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,9 +1,5 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using System.Text.Json.Serialization.Metadata;
-using WebAuthnIssue.Client;
-using System.Text.Json.Serialization;
-using Fido2NetLib;
 
 namespace WebAuthnIssue.Client
 {
@@ -15,19 +11,9 @@ namespace WebAuthnIssue.Client
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddSingleton(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
         }
     }
-
-    [JsonSerializable(typeof(AssertionOptions))]
-    [JsonSerializable(typeof(AuthenticatorAssertionRawResponse))]
-    [JsonSerializable(typeof(AuthenticatorAttestationRawResponse))]
-    [JsonSerializable(typeof(CredentialCreateOptions))]
-    public partial class FidoBlazorSerializerContext : JsonSerializerContext
-    {
-    }
-
-
 }
---
+++ Fido2.Models/Serialization/FidoSerializer.cs
@@ -0,0 +1,23 @@
+﻿// Stratus 21 Addition
+
+namespace Fido2NetLib.Serialization
+{
+    using System.Text.Json;
+
+    public static class FidoSerializer
+    {
+        private static readonly FidoModelSerializerContext context = new FidoModelSerializerContext();
+
+        public static T Deserialize<T>(string json) where T : class
+        {
+            var deserializedObject = JsonSerializer.Deserialize(json, typeof(T), context);
+            return (T)deserializedObject;
+        }
+
+        public static string Serialize<T>(string json) where T : class
+        {
+            var serializedObject = JsonSerializer.Serialize(json, typeof(T), context);
+            return serializedObject;
+        }
+    }
+}
